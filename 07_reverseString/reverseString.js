b808d7af8c7e66edde3c7f10a87092b978fe1bb0
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-01-16 15:07:17 -0300

    Finish exercise TheOdinProject#7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let reversed = str.split('').reverse().join('')
+  return reversed
 };
+
+reverseString('hello there')
 
 // Do not edit below this line
 module.exports = reverseString;
