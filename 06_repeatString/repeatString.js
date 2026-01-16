bd8ec96fc4433fbcf23dce8419bde4bf5192ef8e
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-01-16 14:49:46 -0300

    Finish exercise TheOdinProject#6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
+function repeatString(str, num){
+  let strValue = '' ;
+  
+  if (num < 0) return strValue = 'ERROR'
+  for(let i = 0; i < num; i++){
+    strValue += str
+  }
+  return strValue
+}
 
 // Do not edit below this line
 module.exports = repeatString;
