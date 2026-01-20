431c3599721038931a362f22619d39f780dda5b9
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-01-20 12:01:23 -0300

    Finish exercise TheOdinProject#8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+  for(let i = 0; i < num.length; i++){
+    arr = arr.filter(index => index !== num[i])
+  }
+  return arr;
 };
 
 // Do not edit below this line
