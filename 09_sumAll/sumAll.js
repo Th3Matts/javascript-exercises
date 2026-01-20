c88eece74180eeb9d5d81d8514b7e722fc909a24
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-01-20 16:23:22 -0300

    Fix sumAll typo

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,6 @@
 const sumAll = function(min, max) {
-  if( !Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0 ) return 'ERROR'
+  if( !Number.isInteger(min) || !Number.isInteger(max) ) return 'ERROR'
+  if(min < 0 || max < 0) return 'ERROR'
   
   let sum = 0
   if(min > max){
