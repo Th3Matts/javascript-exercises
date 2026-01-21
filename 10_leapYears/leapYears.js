826636f38d677af7b1415147a20170234626de1e
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-01-21 14:32:51 -0300

    Finish exercise TheOdinProject#10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  /*
+  Leap Year rules: if a year is divisible by 4 or if is a century and
+   is divisible by 400 that year is a leap year.
+  */
+  const isCentury = year % 100 === 0;
 
+  const divisibleByFourHundred = year % 400 === 0;
+
+  if(isCentury) return divisibleByFourHundred;
+  
+  return year % 4 === 0
 };
 
 // Do not edit below this line
