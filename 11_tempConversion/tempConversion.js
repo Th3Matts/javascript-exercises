7ba5c0cba118b0d3f68558d02c25d8f2163d0891
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-01-22 13:04:52 -0300

    Finish exercise TheOdinProject#11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //Converts Fahrenheit to Celsius
+  return +((temp - 32) * (5/9)).toFixed(1); 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //Converts Celsius to Fahrenheit
+  return +((temp * 9/5) + 32).toFixed(1); 
 };
 
 // Do not edit below this line
