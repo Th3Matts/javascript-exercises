49daaac614fad153c6091593f3278321132fa4fe
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-06-15 14:17:00 -0300

    Finish javascript exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const palindrome = string.toLowerCase()
+  .split('')
+  .filter( alpha =>  alpha.match(/[a-z0-9]/) )
+  .join('');
 
+  const reversed = palindrome.split('').reversed().join('');
+  
+  return palindrome === reversed;
 };
 
 // Do not edit below this line
