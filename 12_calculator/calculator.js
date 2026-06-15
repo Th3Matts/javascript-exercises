f2aed8b04201599e473f3c992e494ea11cb8c4a1
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-06-15 10:14:19 -0300

    Finish javascript exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) return 0
+	return arr.reduce( (sum, acc) => sum + acc);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, acc) => sum * acc);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  
+  if (n === 0) return result;
+
+  for(let i = 2; i <= n; i++){
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
