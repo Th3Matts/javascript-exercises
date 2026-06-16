d56199a6692deed1a23008e129a5fe606fc1c5ce
Matheus Rodrigues <matrodriguessilva@gmail.com>
2026-06-16 08:26:24 -0300

    Correct javascript exercise 13

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  n = Number(n);
+  if ( n < 0 ) return "OOPS";
+  
+  const fibo = [0, 1];
+  
+  for ( let i = 2; i <= n; i++) {
+    fibo.push(fibo[i - 1] +  fibo[i - 2]);
+  }
+  
+  return fibo[n];
 };
 
 // Do not edit below this line
